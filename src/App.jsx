c92d99a1d01347348e1b01dc8b3251f51bf95b4e
Vedant Patil <vedant1346@gmail.com>
2024-03-04 23:11:07 +0530

    Volunteer Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Login from "./pages/Login";
 import Landing from "./pages/Landing";
+import Home from "./pages/Home";
+import VolProfile from "./pages/VolProfile";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path="" element={<Landing />} />
           <Route path="/Login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/VolProfile" element={<VolProfile />} />
         </Routes>
       </Router>
     </>
