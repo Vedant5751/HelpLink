55bcf98636d13a06fd5e73af366ccab6705dee5a
Vedant Patil <vedant1346@gmail.com>
2024-03-06 00:40:27 +0530

    Linking Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import HomeNav from "../components/HomeNav"; 
+import HomeHero from "../components/HomeHero";
+import Projects from "../components/Projects";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div><Link to="/volprofile">
-      <button>
-        Profile
-      </button> </Link>
-    </div>
-  )
+    <>
+      <HomeNav />
+      <HomeHero />
+      <Projects />
+    </>
+  );
 }
