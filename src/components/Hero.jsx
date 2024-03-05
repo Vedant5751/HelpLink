fc8201d467dc119ab83febbe491932712e62bede
Vedant Patil <vedant1346@gmail.com>
2024-03-06 00:57:40 +0530

    Redirecting Landing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,31 +23,22 @@ export default function Hero() {
                 platform. From recruitment to task assignment and reporting,
                 streamline your operations and make a bigger impact.
               </p>
-              <Link
-                to="/login"
-                className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:hover:bg-gray-700 dark:focus:ring-primary-900"
-              >
-                Get started
-                <svg
-                  className="w-5 h-5 ml-2 -mr-1"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
+              <Link to="/login">
+                <a
+                  href="#"
+                  className="inline-flex mr-4 items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
                 >
-                  <path
-                    fillRule="evenodd"
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
+                  Join as Volunteer
+                </a>
               </Link>
-              <a
-                href="#"
-                onClick={() => scrollTo("about")}
-                className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-              >
-                Learn More
-              </a>
+              <Link to="/ColLogin">
+                <a
+                  href="#"
+                  className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+                >
+                  Join as Collaborator
+                </a>
+              </Link>
             </div>
             <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
               <img
