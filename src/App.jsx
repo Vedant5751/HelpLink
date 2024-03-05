00ece737aa852df157d602f70750ee12c3d99ea1
Vedant Patil <vedant1346@gmail.com>
2024-03-05 08:19:30 +0530

    Collaborator Profile

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Landing from "./pages/Landing";
 import Home from "./pages/Home";
 import VolProfile from "./pages/VolProfile";
+import ColProfile from "./pages/ColProfile";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/Login" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/VolProfile" element={<VolProfile />} />
+          <Route path="/ColProfile" element={<ColProfile />} />
         </Routes>
       </Router>
     </>
